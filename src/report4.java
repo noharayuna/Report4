0c2de39da95d8b0b22f52f898a3ece291e197580
Yuna Nohara <e195747@ie.u-ryukyu.ac.jp>
2019-12-09 13:39:23 +0900

    step2

--- src/report4.java
+++ src/report4.java
@@ -1,7 +1,14 @@
 public class report4 {
     public static void main(String[] args){
-        String str = null;
-        System.out.println(str.length());
+        try {                     //tryの中でエラーが起きた時↓
+            String str = null;
+            System.out.println(str.length());
+
+        } catch(NullPointerException e){
+            System.out.println("NullPointerException"+"が発生");
+            System.out.println(e.getMessage());
+        }
+
     }
 
     }
